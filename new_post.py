07a7a36ac46cdd93fa71f80357cfb233318cba97
Lewis Tian <taseikyo@gmail.com>
2020-11-13 09:05:00 +0800

    update: add update_readme()

--- new_post.py
+++ new_post.py
@@ -11,6 +11,7 @@
 
 import os
 import time
+import random
 
 TEMPLATE = """
 > @Author  : Lewis Tian (taseikyo@gmail.com)
@@ -43,7 +44,6 @@ TEMPLATE = """
 ## share [⬆](#weekly-{current_id})
 
 [readme](../README.md) | [previous]({previous_post}) | [next]({next_post})
-
 """
 
 
@@ -84,6 +84,61 @@ def has_read_this_url(url: str = ""):
     print(f"{url} has not been read!")
 
 
+def update_readme():
+    """
+    尝试使用 GitHub Action 自动更新 readme
+    """
+    with open("README.md", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    for i, x in enumerate(lines):
+        if x.find("Weeklys") > 0:
+            start = i + 2
+            break
+
+    # GitHub Action 会出现乱序的情况，很奇怪
+    files = os.listdir("weekly")
+    files.sort()
+    print(files)
+
+    word = random.choice(
+        [
+            "OK",
+            "Wow",
+            "Nice",
+            "Amazing",
+            "Excellent",
+            "Well done",
+            "Outstanding",
+        ]
+    )
+    lines[start] = f"{word}! {len(files)} posts in total. Keep going!\n"
+
+    # 表格每行 5 个，不足 5 个补空
+    cnt = 0
+    one_line = []
+    all_line = []
+    for file in files:
+        one_line.append(f" [{file.split('.')[0]}](weekly/{file}) ")
+        cnt += 1
+        if cnt == 5:
+            all_line.append(one_line)
+            one_line = []
+            cnt = 0
+
+    if 0 < cnt < 5:
+        one_line += [" " for _ in range(cnt, 6)]
+        all_line.append(one_line)
+
+    table = "\n".join([f"|{'|'.join(x)}|" for x in all_line])
+
+    license = "\n\n## License\n\nCopyright (c) 2020 Lewis Tian. Licensed under the MIT license.\n"
+
+    with open("README.md", "w", encoding="utf-8") as f:
+        f.write("".join(lines[: start + 4]))
+        f.write(table)
+        f.write(license)
+
+
 if __name__ == "__main__":
-    new_post()
-    has_read_this_url("https://github.com/taseikyo/")
+    update_readme()
