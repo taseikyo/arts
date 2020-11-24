6a7ed3b22cc37c201f530a281d39042a4026cd51
Lewis Tian <taseikyo@gmail.com>
2020-11-24 20:09:17 +0800

    algo (1), tip (1)

---
+++ code/leetcode_0222.go
@@ -0,0 +1,46 @@
+/**
+ * @date    2020-11-24 16:17:44
+ * @authors Lewis Tian (taseikyo@gmail.com)
+ * @link 	github.com/taseikyo
+ */
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + countNodes(root.Left) + countNodes(root.Right);
+}
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	level := 0
+	for node := root; node.Left != nil; node = node.Left {
+		level++
+	}
+	return sort.Search(1<<(level+1), func(k int) bool {
+		if k <= 1<<level {
+			return false
+		}
+		bits := 1 << (level - 1)
+		node := root
+		for node != nil && bits > 0 {
+			if bits&k == 0 {
+				node = node.Left
+			} else {
+				node = node.Right
+			}
+			bits >>= 1
+		}
+		return node == nil
+	}) - 1
+}
