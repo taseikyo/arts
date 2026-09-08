877420094837b220638440b30a750fd681d17f5b
taseikyo <taseikyo@gmail.com>
2026-09-08 16:26:05 +0800

    update pandoc version

--- epub.sh
+++ epub.sh
@@ -21,7 +21,7 @@ if ! type pandoc >/dev/null 2>&1; then
 	# 而使用的模板有个更新需要使用 3.2.1+ 版本的 pandoc，因此需要手动安装
 	# https://github.com/Wandmalfarbe/pandoc-latex-template/issues/405
 	sudo apt install -y curl
-	tag=3.7
+	tag=3.11
 	wget https://github.com/jgm/pandoc/releases/download/$tag/pandoc-$tag-1-amd64.deb
 	sudo dpkg -i pandoc-$tag-1-amd64.deb >/dev/null
 fi
